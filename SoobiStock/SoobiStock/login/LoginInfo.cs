27284460d2a321c48e32fed15a5a17460903b105
ryan <j.soobin@gmail.com>
2019-07-22 01:52:39 +0900

    added login info

---
+++ SoobiStock/SoobiStock/login/LoginInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoobiStock.login
+{
+    public class LoginInfo
+    {
+        public string userid { internal set; get; }
+        public string passwd { internal set; get; }
+    }
+}
